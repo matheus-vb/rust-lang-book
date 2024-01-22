1299e727e77c678b45d8693dc0baec7a0127f1a9
Matheus Batista <matheus.vbatista04@gmail.com>
2024-01-22 20:34:02 -0300

    add base guessing game logic

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,28 @@
+use rand::Rng;
+use std::cmp::{Ord, Ordering};
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    println!("Input your number below: ");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Error reading input");
+
+    let guess: u32 = guess.trim().parse().expect("Please type in a number!");
+
+    println!("Your guessed: {}", guess);
+
+    let secret = rand::thread_rng().gen_range(1, 101);
+
+    match guess.cmp(&secret) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You won!")
+    }
+
+    println!("The number was {}", secret);
 }
